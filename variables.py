a6eaab121ad06031fd5f644e2d0099eb5e9ce5f5
priyanka.jha <pj.priyanka1995@gmail.com>
2020-07-16 09:35:05 +0530

    Variables_section completes here!!

---
+++ variables.py
@@ -0,0 +1,36 @@
+'''
+Variables are just like little boxes in computer-memory that has a name and stores some value in it.
+We store data in it and save it by giving some name.
+To retrieve those values we should use the 'variable-name'.
+variable has two things:
+1. name
+2. value
+name=value(assignment operator in python is '=')
+
+Variables in python are 'case-sensitive'
+
+Variables are dynamically-typed(The variable type can change through-out our program) which is not in case of other programming lang.
+In the below code we assigned an int value to the variable and the data-type
+of the variable 'number' becomes 'int'.
+We again assigned  a string value to the variable and its data-type became
+'string' type.
+This is what we mean by dynamically-typed.
+
+Use lowercase variable names, with underscores(_) for spaces.
+
+Q- Why do we use variable?
+1. To keep useful or important data for future.
+2. To make our code more organised.
+3. To make our code easier to change
+
+
+
+'''
+
+number=1
+print(number)
+"type() function is used to check the data-type of variable"
+print(type(number))
+number='Priyanka'
+print(number)
+print(type(number))
